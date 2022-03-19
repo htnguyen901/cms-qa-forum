60bbc32dd833a393dce66150ea314b138b9c7992
Danzo52-best <62093652+Danzo52-best@users.noreply.github.com>
2022-03-19 19:04:40 +0700

    update role admin and qa-manager in foler area

--- CMSFinal/CMSFinal/App_Start/RouteConfig.cs
+++ CMSFinal/CMSFinal/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace CMSFinal
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "CMSFinal.Controllers" }
             );
         }
     }
---
+++ CMSFinal/CMSFinal/Areas/QaManager/QaManagerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CMSFinal.Areas.QaManager
+{
+    public class QaManagerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "QaManager";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "QaManager_default",
+                "QaManager/{controller}/{action}/{id}",
+                new { action = "Index", Controller="Home", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
