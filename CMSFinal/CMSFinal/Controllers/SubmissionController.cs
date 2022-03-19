bdd9c355d768ea31c60fc54548327b63611bf6d1
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2022-03-20 01:54:16 +0700

    add department model

--- CMSFinal/CMSFinal/Controllers/SubmissionController.cs
+++ CMSFinal/CMSFinal/Controllers/SubmissionController.cs
@@ -46,7 +46,7 @@ namespace CMSFinal.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CategoryId,CategoryName")] Submission submission)
+        public ActionResult Create([Bind(Include = "SubmissionId,SubmissionName,SubmissionDescription, ClosureDate, FinalClosureDate")] Submission submission)
         {
             if (ModelState.IsValid)
             {
