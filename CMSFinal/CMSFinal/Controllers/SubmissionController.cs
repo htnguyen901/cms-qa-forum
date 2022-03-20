39d5faa0b24e69aa9abf6cd1a119685059be7d3f
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2022-03-20 18:23:31 +0700

    rollback package

--- CMSFinal/CMSFinal/Controllers/SubmissionController.cs
+++ CMSFinal/CMSFinal/Controllers/SubmissionController.cs
@@ -57,5 +57,71 @@ namespace CMSFinal.Controllers
 
             return View(submission);
         }
+
+        // GET: Submission/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Submission submission = db.Submissions.Find(id);
+            if (submission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(submission);
+        }
+
+        // POST: Category/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SubmissionId,SubmissionName,SubmissionDescription, ClosureDate, FinalClosureDate")] Submission submission)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(submission).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(submission);
+        }
+
+        // GET: Submission/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Submission submission = db.Submissions.Find(id);
+            if (submission == null)
+            {
+                return HttpNotFound();
+            }
+            return View(submission);
+        }
+
+        // POST: Submission/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Submission submission = db.Submissions.Find(id);
+            db.Submissions.Remove(submission);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
